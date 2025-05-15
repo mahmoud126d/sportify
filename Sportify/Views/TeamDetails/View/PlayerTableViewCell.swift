bc0145fec5d4f29d1f0c4ecc03986b6994ea7176
mahmoud126d <mahmouud.salah2@gmail.com>
2025-05-15 15:21:14 +0300

    team details screen ui

---
+++ Sportify/Views/TeamDetails/View/PlayerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerTableViewCell.swift
+//  Sportify
+//
+//  Created by Macos on 15/05/2025.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
